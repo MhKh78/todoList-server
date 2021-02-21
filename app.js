243261fdb5501885b61df21fddfa146cb097a71f
MhKh78 <mhkh431@gmail.com>
2021-02-21 14:12:38 +0330

    cors added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const errorController = require("./controllers/errorController");
 const AuthRoutes = require("./routes/AuthRoutes");
 const TodoRoutes = require("./routes/TodoRoutes");
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json({ limit: "10kb" }));
 app.use(morgan("dev"));
+app.use(cors());
 app.use("/api/v1/users", AuthRoutes);
 app.use("/api/v1/todos", TodoRoutes);
 
